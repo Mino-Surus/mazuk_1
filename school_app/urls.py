f719dfea0a913ef0edc17cf9025cea925620ae15
Студент группы IS <is@sielom.local>
2025-11-17 10:38:03 +0500

    Korean

--- school_app/urls.py
+++ school_app/urls.py
@@ -2,5 +2,6 @@ from school_app import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.root)
+    path('', views.root),
+    path('student', views.students)
 ]
--- school_app/views.py
+++ school_app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def root(request):
     return render(request, 'main.html') 
+
+def students(request):
+    return render(request, 'student.html')
